b870d7628a840dc99f4904abd45fb418d861864f
Şeyda Zenler <seydaazenler@gmail.com>
2022-03-08 10:55:20 +0300

    First Commit
    Katmanlı mimari oluşturuldu.

---
+++ CoreAPI_DAL/MyContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreAPI_DAL
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
---
+++ CoreAPI_EL/Entities/Assignment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CoreAPI_EL.Entities
+{
+    [Table("Assignments")]
+    public class Assignment
+    {
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Column(Order = 2)]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        [StringLength(500,MinimumLength =2,ErrorMessage =("Görev tanımı en az 2 en fazla 500 karakter olmalıdır!"))]
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
